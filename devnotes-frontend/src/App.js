5e66052761986d83a42cc74d9e121210553cf4c1
anupama <anupamagadde@yahoo.com>
2020-05-16 12:17:34 -0500

    add show route

--- devnotes-frontend/src/App.js
+++ devnotes-frontend/src/App.js
@@ -7,6 +7,7 @@ import NavBar from './components/NavBar.js'
 import Home from './components/Home.js'
 import NotesContainer from './containers/NotesContainer'
 import NewNoteForm from './containers/NewNoteForm'
+import NoteShow from './containers/NoteShow'
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
          <Route exact path="/" component={Home} />
          <Route exact path="/notes" component={NotesContainer} />
          <Route exact path="/notes/new" component={NewNoteForm}/>
+         <Route exact path="/notes/:id" component={NoteShow} />
       </Switch>
       
     </div>
